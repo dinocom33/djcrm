6edb1b95cc96c11197ae8229d53a0220c731c1f8
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-09-20 15:36:15 +0300

    Added automatically sent password reset email to organization owner after creation

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
          name='password-reset'),
     path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/',
-         PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+        template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'),
          name='password_reset_complete'),
 ]
