b7dab50c1aa66f6de1fbb213e5139594086aa957
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-10-23 12:39:02 +0300

    Added delete agent functionality

--- apps/account/models.py
+++ apps/account/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
         _("email address"),
         unique=True,
         validators=[
-            validators.EmailValidator(message='Please enter a valid email address'),
+            validators.EmailValidator(message='Please enter a valid email address!!!!'),
         ]
     )
 
